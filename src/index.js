9e65dceefec1a3761a61348d7145605b2f1ccc81
Mateusz Piasecki <55910795+N3cker@users.noreply.github.com>
2022-09-13 16:17:00 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-<<<<<<< Updated upstream
-import './sass/index.scss'
-=======
 import './sass/main.scss';
 import './sass/index.scss';
 import './js/background.js';
 import './js/dateTime.js';
 import './js/additions.js';
 import './js/searchCity.js';
->>>>>>> Stashed changes
